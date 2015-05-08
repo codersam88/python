7290f907ebabdeb78a34a56ca5ead8f43fa15d70
sampath <sampath.skt@gmail.com>
2015-05-08 16:06:13 +0530

    bell file added

--- Python/src/bellFord.py
+++ Python/src/bellFord.py
@@ -4,6 +4,12 @@ def bellFord():
     nwgraph = adjacencyListW.makeAdjListW()
     
     for i in nwgraph:
-        
+	 print('hello')
+	 print(str(i.name)+' ',end='')
+	 for j in i.adj:
+	       print(str(j)+' ',end='')
+	  print()
+
+	        
 
 bellFord()
